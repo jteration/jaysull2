2cabcd86404938f8d88a052b40158374f8428256
Jacob Sullivan <jay@jaysull.dev>
2021-05-05 13:57:57 -0500

    Add About page

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,20 @@
 <template>
-	<div class="about">
-		<h1>This is an about page</h1>
-	</div>
+	<section id="about">
+		<h1>{{ $t("pages.About.pageTitle") }}</h1>
+		<p>{{ $t("pages.About.firstParagraph") }}</p>
+		<p>
+			{{ $t("pages.About.secondParagraph") }}
+			<cite>{{ $t("pages.About.bookTitle") }}</cite>
+			{{ $t("pages.About.bookAuthors") }}
+		</p>
+		<p>{{ $t("pages.About.thirdParagraph") }}</p>
+	</section>
 </template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+	name: "About"
+});
+</script>
