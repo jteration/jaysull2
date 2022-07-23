dd3b71d4627e7cfc0c121e455e6babb7b73b3ce3
Jacob Sullivan <jay@jaysull.dev>
2022-07-22 22:43:19 -0700

    Update about

--- src/views/About.vue
+++ src/views/About.vue
@@ -2,16 +2,9 @@
   <section id="about">
     <h1>{{ $t("pages.About.pageTitle") }}</h1>
     <p>{{ $t("pages.About.firstParagraph") }}</p>
-    <p>
-      {{ $t("pages.About.secondParagraph") }}
-      <cite>{{ $t("pages.About.bookTitle") }}</cite>
-      {{ $t("pages.About.bookAuthors") }}
-      <cite>{{ $t("pages.About.thirdParagraph") }}</cite>
-      {{ $t("pages.About.secondBookAuthor") }}
-      {{ $t("pages.About.secondParagraphCont") }}
-    </p>
+    <p>{{ $t("pages.About.secondParagraph") }}</p>
+    <p>{{ $t("pages.About.thirdParagraph") }}</p>
     <p>{{ $t("pages.About.fourthParagraph") }}</p>
-    <p>{{ $t("pages.About.fifthParagraph") }}</p>
   </section>
 </template>
 
