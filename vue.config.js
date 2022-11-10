63cae61ea7e836ed02bb4dc6c7bb41239887c6a4
Jacob Sullivan <jay@jaysull.dev>
2022-11-10 06:23:23 -0800

    Update dependencies

--- vue.config.js
+++ vue.config.js
@@ -4,7 +4,7 @@ module.exports = {
       Object.assign(definitions[0], {
         __VUE_I18N_FULL_INSTALL__: JSON.stringify(true),
         __INTLIFY_PROD_DEVTOOLS__: JSON.stringify(false),
-        __VUE_I18N_LEGACY_API__: JSON.stringify(true)
+        __VUE_I18N_LEGACY_API__: JSON.stringify(false)
       });
 
       return definitions;
